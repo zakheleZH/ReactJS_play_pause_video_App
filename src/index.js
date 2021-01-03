ee123d87afed390fde3cf3e5332eaa29b0f27b49
zakhele khumalo <zakhele.khumalo.ms@gmail.com>
2021-01-03 18:19:30 +0200

    React video play and pause
    This React JS application Allow you to play and pause a video

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { Component } from "react";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import video from '../src/assets/Aerial Shot Of Sunset.mp4';
+
+
+
+
+
+
+
+
+
+class VideoPlayer extends React.Component{
+
+  constructor(props){
+
+    super(props);
+
+    this.videoRef=React.createRef();
+
+  }
+
+
+
+  playVideo=()=>{
+
+    this.videoRef.current.play();
+
+  }
+
+
+
+  pauseVideo=()=>{
+
+    this.videoRef.current.pause();
+
+  }
+
+  render(){
+
+    return(
+
+      <div>
+
+        <video ref={this.videoRef} width="300" height="200" controls>
+
+          <source src={video} type="video/mp4"></source>
+
+        </video>
+
+        <div>
+
+           <button onClick={this.playVideo}>Play</button>  
+
+           <button onClick={this.pauseVideo}>Pause</button>  
+
+        </div>        
+
+      </div>
+
+    );
+
+  }
+
+}
+const element=<VideoPlayer></VideoPlayer>
+ReactDOM.render(element,document.getElementById("root"));
